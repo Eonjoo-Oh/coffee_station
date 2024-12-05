6c7f8dea4bfe426504ba61e74ec48ee0a0941a1a
Eonjoo-Oh <eoh@student.42seoul.kr>
2024-12-05 12:32:37 +0900

    home header 2단 타이핑 효과 적용

--- src/components/TitleComponent.js
+++ src/components/TitleComponent.js
@@ -1,13 +1,34 @@
-import React from "react";
-import '../styles/titleComponent.css'
+import React, { useEffect } from "react";
+import "../styles/titleComponent.css";
 
-const TitleComponent = ({title1, title2}) => {
-	return (
-		<div className="titleComponent">
-			<h1 className="titleComponent--1">{title1}</h1>
-			<h1 className="titleComponent--2">{title2}</h1>
-		</div>
-	)
-}
+const TitleComponent = ({ firstTitle, secondTitle }) => {
+	useEffect(() => {
+		let firstTyping = document.querySelector('#firstTitle');
+		
+		if (secondTitle) {
+			let secondTyping = document.querySelector('#secondTitle');
+			secondTyping.style.display = 'none';
+			firstTyping.addEventListener('th.endType', function(e) {
+				setTimeout(() => {
+					secondTyping.style.display = 'block';
+					window.TypeHangul.type('#secondTitle', {intervalType: 76});
+				}, 500);
+			});
+		}
+		if (window.TypeHangul) {
+			window.TypeHangul.type("#firstTitle", {intervalType: 80});
+		} else {
+			console.error("TypeHangul이 로드되지 않았습니다.");
+		} // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div className="titleComponent">
+      <h1 className="titleComponent--1" id="firstTitle">{firstTitle}</h1>
+      <h1 className="titleComponent--2" id="secondTitle">{secondTitle}</h1>
+    </div>
+  );
+};
 
 export default TitleComponent;
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ const Home= () => {
 	return (
 		<div className="home">
 			<div className="home__title">
-				<TitleComponent title1="지친 현대인을 위한" title2="도심 속 에너지 충전소" />
+				<TitleComponent firstTitle="지친 현대인을 위한" secondTitle="도심 속 에너지 충전소" />
 				<div className="home__title--logo">
 					<img src={textLogo} alt="coffee station"></img>
 				</div>
